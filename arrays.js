2b88152c4ef711741d12ddf62517a346acdd374f
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-05 02:26:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,42 @@ function destructivelyAddElementToBeginningOfArray(sourcearray, element)
   var newarray = sourcearray.unshift(element);
   return newarrayl
 }
+
+function addElementToEndOfArray(sourcearray, element)
+{
+    return [...sourcearray, element];
+}
+
+function addElementToEndOfArray(sourcearray, element)
+{
+    var newarray = sourcearray.push(element)
+    return newarray;
+}
+
+function accessElementInArray(sourcearray, index)
+{
+    return sourcearray[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(sourcearray)
+{
+  sourcearray.shift();
+}
+
+function removeElementFromBeginningOfArray(sourcearray)
+{
+  return sourcearray.slice(1);
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(sourcearray)
+{
+    sourcearray.pop();
+    return sourcearray;
+}
+
+function removeElementFromEndOfArray(sourcearray)
+{
+  return sourcearray.slice(0, sourcearray.length - 1)
+}
+
